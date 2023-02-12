e011e4d2381b3a212b61891c0149d6c6917d0386
Paweł Wlezień <pawel.wlezien@gmail.com>
2023-02-12 13:13:58 +0100

    local timezone, better status

--- time_converter.go
+++ time_converter.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"time"
 	"fmt"
+	"time"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"golang.design/x/clipboard"
 )
@@ -19,26 +20,31 @@ type TimeConverter struct {
 }
 
 func (i *TimeConverter) Make() {
+	// get local timezone shift
+	localTzName, offset := time.Now().Zone()
+	
+
 	i.items = append(i.items, MakeTimestampItemsSet("Unix", Unix, time.UTC, i.Update))
 	i.items = append(i.items, MakeTimestampItemsSet("UTC", RFC3339, time.UTC, i.Update))
-	i.items = append(i.items, MakeTimestampItemsSet("Local", RFC3339, time.Local, i.Update))
-	i.items = append(i.items, MakeTimestampItemsSet("EST", RFC3339, time.FixedZone("EST", -5*60*60), i.Update))
-	i.items = append(i.items, MakeTimestampItemsSet("PST", RFC3339, time.FixedZone("PST", -8*60*60), i.Update))
+	i.items = append(i.items, MakeTimestampItemsSet(fmt.Sprintf("Local: %s (%d:00)", localTzName, offset/3600), RFC3339, time.Local, i.Update))
+	i.items = append(i.items, MakeTimestampItemsSet("EST (-5:00)", RFC3339, time.FixedZone("EST", -5*60*60), i.Update))
+	i.items = append(i.items, MakeTimestampItemsSet("PST (-8:00)", RFC3339, time.FixedZone("PST", -8*60*60), i.Update))
 	i.status = widget.NewLabel("")
 	i.Update(time.Now())
 	i.SetStatus("Ready")
 	i.nowButton = widget.NewButton("Now !", func() {
 		i.Update(time.Now())
+		i.SetStatus("Updated to now")
 	})
-	i.fromCliboardButton = widget.NewButton("From Clipboard", func() {
+	i.fromCliboardButton = widget.NewButtonWithIcon("", theme.ContentPasteIcon(), func() {
 		clipboardContent := string(clipboard.Read(clipboard.FmtText))
 
 		t, err := PraseStringToTime(clipboardContent)
 		if err == nil {
 			i.Update(t)
+			i.SetStatus("Updated from clipboard")
 			return
 		}
-
 		i.SetStatus("Clipboard content is not a valid timestamp")
 	})
 }
