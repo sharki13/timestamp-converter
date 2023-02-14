a8b6dcf668c37b4e22643eba18479ceca2db3636
sharki13 <pawel.wlezien@gmail.com>
2023-02-15 00:48:47 +0100

    set default window size

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 )
 
@@ -8,7 +9,7 @@ func main() {
 	app := app.New()
 
 	mainWindow := app.NewWindow("Timestamp converter")
-
+	mainWindow.Resize(fyne.NewSize(600, 10))
 	timestampConverter := &TimestampConverter{}
 
 	timestampConverter.SetContent(mainWindow, app)
